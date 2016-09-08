a5abad8c91d6dc6abf8370e3bd7fb2a4867d6c4c
Robin Abrahamsson <robin.abrahamsson25@gmail.com>
2016-09-08 20:44:20 +0200

    Updated the mac-setup script.

---
+++ mac-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#Install hombrew
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew update
+
+#brew install git
+#brew cask install google-chrome
+
+###################
+### setup ZSH and oh-my-zsh
+###################
+
+#brew install zsh zsh-completions
+
+#curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+#Change shell and you should log in and out after this...
+#chsh -s /usr/local/bin/zsh
+
+echo "    ZSH_THEME=pygmalion
+
+    plugins=(git colored-man colorize github jira vagrant virtualenv pip python brew osx zsh-syntax-highlighting)
+" >> ~/.zshrc
+#    # Add env.sh
+#    source ~/Projects/config/env.sh" >> ~/.zshrc
+
+####################
+### setup spacemacs
+####################
+#brew tap d12frosted/emacs-plus
+#brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
+#brew linkapps
+#sudo rm /usr/bin/emacs
+#sudo rm -rf /usr/share/emacs
+
+#git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+#ln -s .spacemacs-template ~/.spacemacs
